f671bd1ae3f3afb42c321b193cd258e162d8a473
AlexMMorar <alex93morar@gmail.com>
2023-03-22 16:48:29 +0200

    get historical data

--- src/main/scala/Papaya.scala
+++ src/main/scala/Papaya.scala
@@ -1,10 +1,11 @@
 import cats.effect.{ExitCode, IO, IOApp}
+import com.papaya.historical.data.HistoricalData
 
 object Papaya extends IOApp {
 
   override def run(args: List[String]): IO[ExitCode] = {
     for {
-      _ <- PriceMonitor.run.foreverM
+      _ <- HistoricalData.run.foreverM
     } yield ExitCode.Success
   }
 
--- src/main/scala/com/papaya/historical/data/HistoricalData.scala
+++ src/main/scala/com/papaya/historical/data/HistoricalData.scala
@@ -1,7 +1,32 @@
 package com.papaya.historical.data
 
+import cats.effect.IO
+import cats.effect.std.Console
+import fs2.Stream
+import io.github.paoloboni.binance.BinanceClient
+import io.github.paoloboni.binance.spot.parameters.v3.KLines
+import io.github.paoloboni.binance.common.{Interval, KLine, SpotConfig}
+
+import scala.concurrent.duration.DurationInt
+
 object HistoricalData {
 
+
+  val config = SpotConfig.Default(
+    apiKey = "***",
+    apiSecret = "***"
+  )
+
+  val kLineQuery = KLines(symbol = "BTCUSDT", interval = Interval.`4h`, startTime = None, endTime = None, limit = 500)
   
+  def run =
+    BinanceClient
+      .createSpotClient[IO](config)
+      .use { client =>
+       client.V3.getKLines(kLineQuery)
+         .evalMap(kline => Console[IO].print(s"This is it!: + ${kline}"))
+         .compile
+         .drain
+      }
 
 }
